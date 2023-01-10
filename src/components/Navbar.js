87835ee6d9356a4b6b5e283b08f27248ff0e86fd
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-01-10 17:46:38 -0600

    added Buttons in navabr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
-import { VStack, Flex, Heading } from "@chakra-ui/react";
+import { VStack, Flex, Heading, Spacer, Button } from "@chakra-ui/react";
 
 function Navbar() {
 	return (
-		<VStack>
-			<Flex>
-				<Heading>Gustavo Sepulveda</Heading>
+		<VStack p={5}>
+			<Flex w="100%">
+				<Heading ml={4} size="md">
+					Gustavo Sepulveda
+				</Heading>
+				<Spacer />
+				<Button colorScheme="purple" variant="ghost">
+					Projects
+				</Button>
+				<Button colorScheme="purple" variant="ghost">
+					Linkedin
+				</Button>
+				<Button colorScheme="purple" variant="ghost">
+					Resume
+				</Button>
 			</Flex>
 		</VStack>
 	);
