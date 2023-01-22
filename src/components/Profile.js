359b4f7432fb3f08b8800738ac07a8b22eb6e216
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-01-21 22:30:01 -0600

    added box and slpit skills

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { FaHtml5, FaCss3, FaJs, FaReact, FaBootstrap, FaNodeJs  } from "react-icons/fa";
+import {
+	FaHtml5,
+	FaCss3,
+	FaJs,
+	FaReact,
+	FaBootstrap,
+	FaNodeJs,
+} from "react-icons/fa";
 import { SiMongodb, SiExpress } from "react-icons/si";
-import { Stack, Flex } from "@chakra-ui/react";
+import { Stack, Flex, Text, Box } from "@chakra-ui/react";
 
 function Profile() {
 	return (
 		<Stack>
 			<Flex>
-				<FaHtml5 size="3rem" />
-				<FaCss3 size="3rem"/>
-				<FaJs size="3rem"/>
-				<FaReact size="3rem"/>
+        <Box>
+				<FaHtml5 size="3rem"></FaHtml5>
+        <Text>HTML</Text>
+				<FaCss3 size="3rem" />
+        <Text>CSS</Text>
+				<FaJs size="3rem" />
+        <Text>JavaScript</Text>
+				<FaReact size="3rem" />
+        <Text>React</Text>
+        </Box>
 
-        <FaNodeJs size="3rem" />
-        <SiMongodb size="3rem" />
-        <SiExpress size="3rem" />
+        <Box>
+				<FaNodeJs size="3rem" />
+        <Text>NodeJS</Text>
+				<SiMongodb size="3rem" />
+        <Text>MongoDB</Text>
+				<SiExpress size="3rem" />
+        <Text>Express</Text>
+        </Box>
 
-        <FaBootstrap size="3rem" />
+        <Box>
+				<FaBootstrap size="3rem" />
+        <Text>BootStrap</Text>
+        </Box>
 			</Flex>
 		</Stack>
 	);
