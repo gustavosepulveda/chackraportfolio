91da09ea8230aad140b5f5316c2a7af79b922b0a
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-02-02 18:10:17 -0600

    added font and theme

---
+++ src/components/Font.js
@@ -0,0 +1,9 @@
+import { Global } from "@emotion/react";
+
+const Font = () => (
+	<Global
+		styles={`@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+    `}
+	/>
+);
+export default Font;
